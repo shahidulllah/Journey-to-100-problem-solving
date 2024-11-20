42d5991d1c99ddeb3d8cf8f0b38b06fdd77c9461
shahidullah177 <mdshahidsumon177@gmail.com>
2024-11-20 22:44:06 +0600

    Problem 2,3 done

---
+++ 100-problems-solving-terget/2-printWindow.js
@@ -0,0 +1,6 @@
+//PROBLEM:===> 02
+//Write a JavaScript program to print the current window contents.
+
+function showCurrentWindow (){
+    window.print();
+}
---
+++ 100-problems-solving-terget/3-getCurrentDate.js
@@ -0,0 +1,20 @@
+//PROBLEM:===> 03
+//Write a JavaScript program to get the current date.
+//Expected Output :
+//mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+let today = new Date()
+let date = today.getDate();
+let month = today.getMonth() + 1;
+let year = today.getFullYear();
+
+if(date < 10){
+    date = "0" + date;
+}
+
+if(month < 10){
+    date = "0" + month;
+}
+
+today = date + "-" + month + "-" + year;
+console.log(today);
