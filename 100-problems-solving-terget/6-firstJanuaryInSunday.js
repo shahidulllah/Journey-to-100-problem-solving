36674c35547ed6aff3ad1d84f7ad52718d4289ee
shahidullah177 <mdshahidsumon177@gmail.com>
2024-11-26 00:51:16 +0600

    1st january is being Sunday

---
+++ 100-problems-solving-terget/6-firstJanuaryInSunday.js
@@ -0,0 +1,14 @@
+//Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.
+
+
+console.log('--------------------');
+
+for(let year = 2014; year<= 2050; year++){
+    const date = new Date(year, 0, 1);
+
+    if(date.getDay() === 0) {
+        console.log("1st January is being a Sunday " + year);
+    }
+}
+
+console.log('--------------------');
