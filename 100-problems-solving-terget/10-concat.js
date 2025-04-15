16ab67c041ff724ac91d783a4542c3b38d4dde83
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-15 23:35:44 +0600

    concat modify

--- 100-problems-solving-terget/10-concat.js
+++ 100-problems-solving-terget/10-concat.js
@@ -1,3 +1,7 @@
+/*Write a function named concat3 that receives 3 strings as parameters (string1, string2, string3) and an additional string named separator.
+
+The function should concatenate the three strings using the provided separator and return the result. */
+
 function concat3(string1, string2, string3, separator) {
   return `${string1}${separator} ${string2}${separator} ${string3}`;
 }
