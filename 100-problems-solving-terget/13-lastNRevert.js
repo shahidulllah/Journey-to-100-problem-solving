617de41d0a9a40adf1761532a7bc88148f4c406c
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-17 21:08:54 +0600

    lastRevertN

---
+++ 100-problems-solving-terget/13-lastNRevert.js
@@ -0,0 +1,12 @@
+/**7.lastNRevert
+Write a function named lastNRevert that receives 2 parameters:
+
+a string named text
+a number - n
+and returns the last n characters of text but in reverse order. */
+
+const lastNumberRevert = (text, number) => {
+  const lastN = text.slice(-number);
+
+  return lastN.split("").reverse().join("");
+};
