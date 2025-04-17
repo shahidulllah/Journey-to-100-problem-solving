645995d3296dbfa0b76e5d7ebd48f444e1de0a2c
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-17 21:19:18 +0600

    get business address

---
+++ 100-problems-solving-terget/14-getAddress.js
@@ -0,0 +1,15 @@
+/**8.getBusinessAddress
+Write a function named getBusinessAddress that receives an Object representing a business as a parameter, and returns a string representation of the it's address. */
+
+function getBusinessAddress(business) {
+  const address = business.address;
+
+  return `${address.street}, ${business.name}, ${address.zipCode}`;
+}
+
+const business = {
+  address: { number: 3, street: "Avenuepark", zipCode: 123500 },
+  name: "100 Functions ltd.",
+};
+
+console.log(getBusinessAddress(business));
