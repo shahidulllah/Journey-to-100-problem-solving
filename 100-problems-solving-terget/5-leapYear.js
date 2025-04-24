3671c90812001efffe360b5517863ba0398d708d
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-24 19:44:42 +0600

    modify more

--- 100-problems-solving-terget/5-leapYear.js
+++ 100-problems-solving-terget/5-leapYear.js
@@ -1,8 +1,7 @@
-//Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. 
-
+//Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
 
 const LeapYearFinding = (year) => {
-    return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
-}
+  return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+};
 
-console.log(LeapYearFinding(2012)); 
+console.log(LeapYearFinding(2012));
