cf6a7a19b3b8cb4c2a70655ea7c2f551a6e54834
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-25 12:46:59 +0600

    modify

--- 100-problems-solving-terget/1-showDateTime.js
+++ 100-problems-solving-terget/1-showDateTime.js
@@ -21,34 +21,43 @@ TO DO:
 //SOLUTION//
 const today = new Date();
 const day = today.getDay();
-const dayList = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+const dayList = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+];
 console.log("Today is : ", dayList[day]);
 
 let hour = today.getHours();
 const minute = today.getMinutes();
 const second = today.getSeconds();
 
-let prepand = (hour >= 12) ? 'PM' : 'AM';
-hour = (hour >= 12) ? hour - 12 : hour;
+let prepand = hour >= 12 ? "PM" : "AM";
+hour = hour >= 12 ? hour - 12 : hour;
 
-if(hour === 0 && prepand === 'PM'){
-    if(minute === 0 && second === 0){
-        hour = 12;
-        prepand = "Noon"
-    }else{
-        hour = 12;
-        prepand = 'PM'
-    }
+if (hour === 0 && prepand === "PM") {
+  if (minute === 0 && second === 0) {
+    hour = 12;
+    prepand = "Noon";
+  } else {
+    hour = 12;
+    prepand = "PM";
+  }
 }
 
-if(hour === 0 && prepand === 'AM'){
-    if(minute === 0 && second === 0){
-        hour = 12;
-        prepand = "Mid-night"
-    }else{
-        hour = 12;
-        prepand = 'AM'
-    }
+if (hour === 0 && prepand === "AM") {
+  if (minute === 0 && second === 0) {
+    hour = 12;
+    prepand = "Mid-night";
+  } else {
+    hour = 12;
+    prepand = "AM";
+  }
 }
 
-console.log("Current Time: "+ hour + ":" + minute + ":" + second + " " + prepand); 
+console.log(
+  "Current Time: " + hour + ":" + minute + ":" + second + " " + prepand
+);
