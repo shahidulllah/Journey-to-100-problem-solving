c8cd8646f536b1ce5fc5d782131599d53b43dba3
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-25 12:47:51 +0600

    '|

--- 100-problems-solving-terget/3-getCurrentDate.js
+++ 100-problems-solving-terget/3-getCurrentDate.js
@@ -3,17 +3,17 @@
 //Expected Output :
 //mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
 
-let today = new Date()
+let today = new Date();
 let date = today.getDate();
 let month = today.getMonth() + 1;
 let year = today.getFullYear();
 
-if(date < 10){
-    date = "0" + date;
+if (date < 10) {
+  date = "0" + date;
 }
 
-if(month < 10){
-    date = "0" + month;
+if (month < 10) {
+  date = "0" + month;
 }
 
 today = date + "-" + month + "-" + year;
