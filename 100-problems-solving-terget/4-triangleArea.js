071adaf18b2cd299200efaa8ae3efe527b7dc91c
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-25 12:48:18 +0600

    triangle

--- 100-problems-solving-terget/4-triangleArea.js
+++ 100-problems-solving-terget/4-triangleArea.js
@@ -7,6 +7,8 @@ const side3 = 7;
 
 const s = (side1 + side2 + side3) / 2;
 
-const area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3))).toFixed(2)
+const area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3))).toFixed(
+  2
+);
 
 console.log(area);
