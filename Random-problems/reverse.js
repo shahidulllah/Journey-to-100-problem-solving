215ced18d02e088763f702fa92b539ed0131c7a7
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-27 22:51:28 +0600

    reverse practice

---
+++ Random-problems/reverse.js
@@ -0,0 +1,14 @@
+const reversString = (data) => {
+    const stringAsArray = data.split(" ");
+
+   const result =  stringAsArray.map(d => {
+    const dataAsArray =  d.split("")
+
+    return dataAsArray.reverse().join("")
+    })
+
+    return result.join(" ");
+}
+
+const string = "My name is Shahidullah";
+console.log(reversString(string));
