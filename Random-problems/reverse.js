e74518a12ffe0ab02d5d09bb814f71924a8fe296
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-27 22:52:00 +0600

    modify

--- Random-problems/reverse.js
+++ Random-problems/reverse.js
@@ -1,14 +1,14 @@
 const reversString = (data) => {
-    const stringAsArray = data.split(" ");
+  const stringAsArray = data.split(" ");
 
-   const result =  stringAsArray.map(d => {
-    const dataAsArray =  d.split("")
+  const result = stringAsArray.map((d) => {
+    const dataAsArray = d.split("");
 
-    return dataAsArray.reverse().join("")
-    })
+    return dataAsArray.reverse().join("");
+  });
 
-    return result.join(" ");
-}
+  return result.join(" ");
+};
 
 const string = "My name is Shahidullah";
 console.log(reversString(string));
