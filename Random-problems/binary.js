3cf7e4d52dac97ef15289d3b3db6d7bb35716264
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-03 23:20:57 +0600

    modify

--- Random-problems/binary.js
+++ Random-problems/binary.js
@@ -2,5 +2,4 @@ function countBits(n) {
   return n.toString(2).split("1").length - 1;
 }
 
-// Example usage
-console.log(countBits(1234)); // Output: 5
+
--- Random-problems/divide.js
+++ Random-problems/divide.js
@@ -2,4 +2,3 @@ const divide = (num1, num2) => {
   return num1 / num2;
 };
 
-console.log(divide(3, 4));
