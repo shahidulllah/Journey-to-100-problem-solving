cc7e9af9f6977ca5e693487c6dafd9954a625295
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-07 19:45:09 +0600

    practice closure

---
+++ Random-problems/closure.js
@@ -0,0 +1,14 @@
+function closure(value2) {
+  const value = "This is value";
+
+  function insideFunc() {
+    const value3 = "This is inside value";
+
+    console.log("Parameter:", value2);
+    console.log("Outer value:", value);
+    console.log("Inside value:", value3);
+  }
+  return insideFunc();
+}
+
+closure("I pass something");
