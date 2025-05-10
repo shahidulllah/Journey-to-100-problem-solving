b3b77165b31cef590521864eaf69e48d949408f7
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-11 02:11:23 +0600

    duplicate

---
+++ Random-problems/DuplicateArray.js
@@ -0,0 +1,13 @@
+//How can you eliminate duplicate values from a JavaScript array?
+
+const arr = [1, 3, 4, 3, 5, 6, 4, 3, 6, 7, 5, 5, 9, 8];
+
+const newArray = arr.reduce((prev, current) => {
+  if (!prev.includes(current)) {
+    prev.push(current);
+  }
+
+  return prev;
+}, []);
+
+console.log(newArray);
