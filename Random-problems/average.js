e8dcbc96eba9fa1713b598cfdbb0b92e1259d1bf
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-21 23:41:19 +0600

    average

---
+++ Random-problems/average.js
@@ -0,0 +1,9 @@
+
+function average(arr) {
+  if (arr.length === 0) return 0;
+  const sum = arr.reduce((total, num) => total + num, 0);
+  return sum / arr.length;
+}
+
+const array = [2, 3, 4, 5];
+console.log(average(array));
