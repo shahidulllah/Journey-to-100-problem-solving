d6e495fdf7ef91740336df25e68e83b362b69744
shahidulllah <mdshahidsumon177@gmail.com>
2025-07-01 02:26:26 +0600

    modifing

--- 100-problems-solving-terget/6-firstJanuaryInSunday.js
+++ 100-problems-solving-terget/6-firstJanuaryInSunday.js
@@ -1,6 +1,5 @@
 //Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.
 
-console.log("--------------------");
 
 for (let year = 2014; year <= 2050; year++) {
   const date = new Date(year, 0, 1);
@@ -10,4 +9,4 @@ for (let year = 2014; year <= 2050; year++) {
   }
 }
 
-console.log("--------------------");
+
