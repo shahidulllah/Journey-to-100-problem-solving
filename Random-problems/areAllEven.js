c960646294eb27482c6ef9f1837425f597ef87d3
shahidulllah <mdshahidsumon177@gmail.com>
2025-11-12 15:47:12 +0600

    working

--- Random-problems/areAllEven.js
+++ Random-problems/areAllEven.js
@@ -1,3 +1,5 @@
+//Write function which takes a array of number and it will filter whether the numbers all even of not
+
 function areAllNumbersEven(numbers) {
   return numbers.every((number) => number % 2 === 0);
 }
