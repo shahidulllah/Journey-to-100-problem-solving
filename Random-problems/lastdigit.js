f84aaef3359e4d8f8b0684228fd3d9541d4c5445
shahidulllah <mdshahidsumon177@gmail.com>
2025-11-18 19:02:12 +0600

    working

--- Random-problems/lastdigit.js
+++ Random-problems/lastdigit.js
@@ -1,11 +1,8 @@
 // Define a function named last_digit with parameters x, y, and z
 function last_digit(x, y, z) {
-  // Check if x, y, and z are greater than 0
   if (x > 0 && y > 0 && z > 0) {
-    // Check if the last digits of x, y, and z are equal
     return x % 10 === y % 10 && y % 10 === z % 10 && x % 10 === z % 10;
   } else {
-    // If any of the numbers is not greater than 0, return false
     return false;
   }
 }
